415214c5fcbb8f286480d45a439852e17aeaffce
mike5941 <mike9159@naver.com>
2024-03-11 12:08:22 +0900

    Terraform

--- primary/network/main.tf
+++ primary/network/main.tf
@@ -1,5 +1,5 @@
 module "azure_example" {
-  source = "github.com/mike5941/az_dr-modules//modules/network"
+  source = "github.com/mike5941/az_dr-modules//network"
   name = "primary-sg"
   location = "Korea Central"
 }
